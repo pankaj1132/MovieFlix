ce2b6d001a0041dc102e066f6278272888cdc722
pankaj1132 <pankaj17469@gmail.com>
2024-05-20 15:08:37 +0530

    commit

---
+++ src/store/homeSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const homeSlice = createSlice({
+  name: 'home',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState:{
+url:{},
+genres:{}
+  },
+     
+
+  
+  reducers: {
+    
+     getApiConfiguration:(state,action)=>{
+          state.url=action.payload;
+     },
+     getGenres:(state,action)=>{
+          state.genres=action.payload;
+     }
+  },
+})
+
+export const {getApiConfiguration,getGenres} = homeSlice.actions
+
+
+
+export default homeSlice.reducer
